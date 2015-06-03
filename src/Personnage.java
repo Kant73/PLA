c82f2c0fc02cfb1e854b7c859fd53c3ce237db25
Julian <Julian@130.190.52.117>
2015-06-03 19:20:04 +0200

    Rectification commit précédent

---
+++ src/Personnage.java
@@ -0,0 +1,10 @@
+
+public abstract class Personnage {
+
+	private int currentX;
+	private int currentY;
+	private String Orientation; /* A changer en type enum N,S,E,O*/
+	private Programme prog;
+	
+	
+}
