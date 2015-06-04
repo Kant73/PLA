3a4f5528b20b1ea9e97a09e5cad53d090421d101
Julian <Julian@130.190.55.102>
2015-06-04 11:26:32 +0200

    Maj signature méthode Personnage

--- src/Personnage.java
+++ src/Personnage.java
@@ -13,5 +13,14 @@ public abstract class Personnage {
 	private Pcardinaux orientation; 
 	private Programme prog;	
 	
+	Personnage(int x, int y){
+		init(x,y);
+	}
+	
+	public void init(int x, int y){}
+	
+	public void printTerm(){}
+	
+	public void execute(Programme p, Terrain t){}
 }
 
