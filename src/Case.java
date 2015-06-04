f98acf0c880d0a69c7168b34c7741d65d8b22ae1
lawchris <law.christopher38@gmail.com>
2015-06-04 11:29:12 +0200

    Actualisation classe Case et Terrain.

--- src/Case.java
+++ src/Case.java
@@ -3,5 +3,14 @@ public class Case {
 	private Status status;
 	private int hauteur;
 	
+	public void printTerm() {
+	}
+	
+	public void setCase(Status s) {
+	}
+	
+	public Case getCase() {
+		return null;
+	}
 	
 }
--- src/Terrain.java
+++ src/Terrain.java
@@ -1,4 +1,19 @@
 
 public class Terrain {
 
+	private int largeur;
+	private int longueur;
+	private int score;
+	private int nbActionsPossible;
+	private int positionInitiale [] [];
+	private int ensembleDeCase [] [];
+	private String orientationInitiale;
+	
+	public Terrain init() {
+		return null;
+	}
+	
+	public void detruire() {
+	}
+	
 }
