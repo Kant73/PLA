ccd38411fda0352f755893ea8aff9e084420262b
P0ppoff <jules.hablot@gmail.com>
2015-06-04 11:34:24 +0200

    méthodes des programmes

--- src/Programme.java
+++ src/Programme.java
@@ -1,9 +1,27 @@
 
-public interface Programme {
+abstract class Programme {
 
 	public int selection;
 	public int nbMaxAction;
 	public String nom;
-	/* ---- Ensemble d'actions*/
+	/* ---- Ensemble d'actions */
+	
+	/* Gestion des actions */
+	public void AjouterAction(Actions a){}
+	
+	public void DeplacerAction(Actions a){}
+	
+	public void SupprimerAction(Actions a){}
+	
+	public void InsererAction(Actions a){}
+	
+	/* Gestion des procedures */
+	public void AjouterProcedure(Procedures a){}
+	
+	public void DeplacerProcedure(Procedures a){}
+	
+	public void SupprimerProcedure(Procedures a){}
+	
+	public void InsererProcedure(Procedures a){}
 	
 }
