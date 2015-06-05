df237a0eb13d7c25a6f1cb99936c2b984bbf9f87
Mugiwara No Luffy <Mugiwara No Luffy@MugiwaraNoLuffy>
2015-06-05 18:19:19 +0200

    Update terrain, creation getter setter

--- src/LightBot/Terrain.java
+++ src/LightBot/Terrain.java
@@ -19,5 +19,69 @@ public class Terrain {
 	
 	public void detruire() {
 	}
+
+	public int getLargeur() {
+		return largeur;
+	}
+
+	public void setLargeur(int largeur) {
+		this.largeur = largeur;
+	}
+
+	public int getLongueur() {
+		return longueur;
+	}
+
+	public void setLongueur(int longueur) {
+		this.longueur = longueur;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public int getNbActionsPossible() {
+		return nbActionsPossible;
+	}
+
+	public void setNbActionsPossible(int nbActionsPossible) {
+		this.nbActionsPossible = nbActionsPossible;
+	}
+
+	public int getxInit() {
+		return xInit;
+	}
+
+	public void setxInit(int xInit) {
+		this.xInit = xInit;
+	}
+
+	public int getyInit() {
+		return yInit;
+	}
+
+	public void setyInit(int yInit) {
+		this.yInit = yInit;
+	}
+
+	public int[][] getEnsembleDeCase() {
+		return ensembleDeCase;
+	}
+
+	public void setEnsembleDeCase(int ensembleDeCase[][]) {
+		this.ensembleDeCase = ensembleDeCase;
+	}
+
+	public Pcardinaux getOrientationInitiale() {
+		return orientationInitiale;
+	}
+
+	public void setOrientationInitiale(Pcardinaux orientationInitiale) {
+		this.orientationInitiale = orientationInitiale;
+	}
 	
 }
