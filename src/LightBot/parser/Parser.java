8a3207c8c7c746b7b8377cfc674d6ebc8d899225
P0ppoff <jules.hablot@gmail.com>
2015-06-08 15:31:55 +0200

    Ajout du parser non focntionnel

---
+++ src/LightBot/parser/Parser.java
@@ -0,0 +1,28 @@
+package LightBot.parser;
+
+import org.w3c.dom.Document;
+
+import LightBot.Niveau;
+
+public class Parser {
+	
+	private Document doc;
+	private Niveau n;
+	
+	public Parser(String fichier){
+		//créer le document à partir du nom du fichier
+	}
+	
+	public void lire(){
+		
+	}
+	
+	public void ecrire(){
+		
+	}
+	
+	public Niveau getNiveau(){
+		return this.n;
+	}
+
+}
