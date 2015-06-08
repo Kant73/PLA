ea23f25fd6aa3acd7919d6710309f30a58117e29
Julian <Julian@130.190.55.148>
2015-06-08 15:12:56 +0200

    Ajout liste d'actions dans Niveau

--- src/LightBot/Niveau.java
+++ src/LightBot/Niveau.java
@@ -2,6 +2,7 @@ package LightBot;
 
 import java.util.ArrayList;
 
+import LightBot.actions.Actions;
 import LightBot.personnage.Personnage;
 
 
@@ -10,19 +11,28 @@ public class Niveau {
 	private ArrayList<Personnage> personnages;
 	private Terrain terrain;
 	private ArrayList<Programme> programmes;
+	private ArrayList<Actions> actions;
 	
 	public Niveau(){
 		this.personnages=new ArrayList<Personnage>();
 		this.programmes=new ArrayList<Programme>();
+		this.actions=new ArrayList<Actions>();
 	}
 	
-
+	public ArrayList<Actions> getActions(){
+		return this.actions;
+	}
+	
 	public ArrayList<Personnage> getPersonnages() {
 		return this.personnages;
 	}
 
 	public void setPersonnages(ArrayList<Personnage> personnages) {
 		this.personnages = personnages;
+	}
+	
+	public void setAction(Actions a){
+		this.actions.add(a);
 	}
 
 	public Terrain getTerrain() {
