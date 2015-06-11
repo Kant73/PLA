89065241f1a8129c6874199aeb71d6b67acd2676
Julian <Julian@130.190.55.53>
2015-06-11 11:17:52 +0200

    Ajout Mode_Jeu

--- src/LightBot/Mode_Jeu.java
+++ src/LightBot/Mode_Jeu.java
@@ -1,13 +1,38 @@
 package LightBot;
 
-import java.awt.List;
+import java.io.File;
+import java.util.ArrayList;
+
+import LightBot.parser.Parser;
 
 
 
 public class Mode_Jeu {	
 
-	private String nom;
-	private List Cases;
-	private List Terrain;
+	private ArrayList<Niveau> niveaux; 
+	
+	public Mode_Jeu(NomMode mode){
+		this.niveaux=new ArrayList<Niveau>();
+		this.setNiveaux(mode);
+	}	
+	
+	public ArrayList<Niveau> getNiveaux(){
+		return this.niveaux;
+	}
+	
+	private void setNiveaux(NomMode mode){
+		if(mode!=null && this.niveaux!=null){
+			File folder = new File(mode.getPath());
+			Parser p;
+		    for (File fichier:folder.listFiles()) {
+		    	String nomFichier=fichier.toString();
+				if (fichier.isFile() && nomFichier.substring(nomFichier.length()-4).equals(".xml")) {
+					p=new Parser(fichier.toString());
+					p.lire();
+					this.niveaux.add(p.getNiveau());
+				}
+		    }
+		}
+	}
 		
 }
