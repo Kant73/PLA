34f2ea1ba0019d3cd06d407138c6634f945a650f
Julian <Julian@130.190.55.53>
2015-06-11 17:44:25 +0200

    Modif Mode_Jeu

--- src/LightBot/Mode_Jeu.java
+++ src/LightBot/Mode_Jeu.java
@@ -11,6 +11,10 @@ public class Mode_Jeu {
 
 	private ArrayList<Niveau> niveaux; 
 	
+	public static void main(String[] pArgs){
+		new Mode_Jeu(NomMode.Basic);
+	}
+	
 	public Mode_Jeu(NomMode mode){
 		this.niveaux=new ArrayList<Niveau>();
 		this.setNiveaux(mode);
@@ -23,14 +27,19 @@ public class Mode_Jeu {
 	private void setNiveaux(NomMode mode){
 		if(mode!=null && this.niveaux!=null){
 			File folder = new File(mode.getPath());
+			ArrayList<String> noms=new ArrayList<String>();			
 			Parser p;
+			
 		    for (File fichier:folder.listFiles()) {
-		    	String nomFichier=fichier.toString();
-				if (fichier.isFile() && nomFichier.substring(nomFichier.length()-4).equals(".xml")) {
-					p=new Parser(fichier.toString());
-					p.lire();
-					this.niveaux.add(p.getNiveau());
-				}
+		    	String nomFichier=fichier.toString();		    	
+				if (fichier.isFile() && nomFichier.substring(nomFichier.length()-4).equals(".xml"))
+					noms.add(nomFichier);
+		    }
+		    noms.sort(String.CASE_INSENSITIVE_ORDER);
+		    for(String nom:noms){
+				p=new Parser(nom);
+				p.lire();
+				this.niveaux.add(p.getNiveau());
 		    }
 		}
 	}
