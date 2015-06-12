276f62278efcb02b1ebe7b4a90ab2bf3968232fb
Julian <Julian@130.190.55.53>
2015-06-13 00:31:07 +0200

    Ordonnanceur: Gestion de plusieurs Robot, 			  Recursivité OK, 			  Gestion du Break OK

--- src/LightBot/Ordonnanceur.java
+++ src/LightBot/Ordonnanceur.java
@@ -2,6 +2,8 @@ package LightBot;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Vector;
 
 import LightBot.personnage.Personnage;
@@ -11,55 +13,86 @@ public class Ordonnanceur {
 
 	private ArrayList<Programme> progs;
 	private Vector<Iterator<Object>> listItActions;
+	private ArrayList<LinkedList<Iterator<Object>>> listFifo;
 	
 	
 	Ordonnanceur(ArrayList<Personnage> persos){
 		this.progs=new ArrayList<Programme>();
 		this.listItActions=new Vector<Iterator<Object>>();
+		this.listFifo=new ArrayList<LinkedList<Iterator<Object>>>();
 		for(int i=0;i<persos.toArray().length;i++){
 			this.progs.add(persos.get(i).getProgramme());
 			this.listItActions.add(persos.get(i).getProgramme().getActions().iterator());
+			this.listFifo.add(new LinkedList<Iterator<Object>>());
+			this.listFifo.get(i).add(this.listItActions.get(i));
 		}
 	}
 	
 	public void run(){
 		try{
-			for(Iterator<Object> it:this.listItActions){	
-				if(it.hasNext()){
-					this.execute(it);
-				}
-			}
+			for(int i=0;i<this.listItActions.toArray().length;i++) //Execute une action pour chaque robot
+				this.execute((Iterator<Object>)this.listItActions.toArray()[i],i);
+			
+			if(isListFifoEmpty())return;
 			this.run();
 		}catch(NullPointerException e){
 			e.printStackTrace();
 			return;		
 		}catch(StackOverflowError e){
 			e.printStackTrace();
-		}catch(NoClassDefFoundError noDef){}
+		}catch(NoClassDefFoundError noDef){	
+		}catch (ArrayIndexOutOfBoundsException aE){//Sorti du terrain
+		}catch (BreakException e) {
+			this.restaureIterator(Integer.parseInt(e.getMessage()));			
+			this.run();
+		}			
 	}
 	
-	private void execute(Iterator<Object> itActions){
+	private void execute(Iterator<Object> itActions,int index) throws ArrayIndexOutOfBoundsException,BreakException{
 		try{			
+			
 			if(itActions.hasNext()){
 				Object obj=itActions.next();
-				
-			    if(obj instanceof Break && ((Break)obj).getCouleur()==((Break)obj).getPersonnage().getCouleur())return;
-				else if(obj instanceof Actions){
+			    if(obj instanceof Break && ((Break)obj).getCouleur()==((Break)obj).getPersonnage().getCouleur()){
+			    	throw new BreakException(Integer.toString(index));
+			    }
+				else if(obj instanceof Actions && !(obj instanceof Break)){
 					int nbLampeAllumee=((Actions)obj).getPersonnage().getTerrain().getNbLampeAllumee();
-					if(nbLampeAllumee >= ((Actions)obj).getPersonnage().getTerrain().getMaxLampe() || ((Actions)obj).getPersonnage().isMort())return;
-					else((Actions)obj).agir();
+					if(nbLampeAllumee >= ((Actions)obj).getPersonnage().getTerrain().getMaxLampe() || ((Actions)obj).getPersonnage().isMort()) throw new ArrayIndexOutOfBoundsException();
+					else{
+						((Actions)obj).agir();
+					}
 				}
-				else if(obj instanceof Programme)((Programme)obj).execute();
+				else if(obj instanceof Programme){
+					this.setNewIterator(((Programme)obj).getActions().iterator(), index);
+				}
+			}else{
+				this.restaureIterator(index);
 			}
 		}catch(StackOverflowError e){
 			e.printStackTrace();
 		}catch(NoClassDefFoundError noDef){
 		}catch(NullPointerException nE){
 			return;
-		}catch(Exception ex){
-			ex.printStackTrace();
 		}
 	}
 	
+	private void setNewIterator(Iterator<Object> iterator,int index){
+		this.listFifo.get(index).add(this.listItActions.get(index));
+		this.listItActions.setElementAt(iterator, index);
+	}
 	
+	private void restaureIterator(int index){
+		if(!this.listFifo.get(index).isEmpty())
+			this.listItActions.setElementAt(this.listFifo.get(index).removeLast(),index);
+	}
+	
+	private boolean isListFifoEmpty(){
+		Boolean bool=true;
+		for(int i=0;i<this.listItActions.toArray().length;i++){
+			bool&=!((Iterator<Object>) this.listItActions.toArray()[i]).hasNext();
+			bool&=((LinkedList<Iterator<Object>>)this.listFifo.get(i)).size()==0;
+		}			
+		return bool;
+	}
 }
