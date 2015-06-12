48ea3ad20d2cd53925ab18cd79ac8bed3385e0d1
Mugiwara No Luffy <Mugiwara No Luffy@MugiwaraNoLuffy>
2015-06-12 14:38:50 +0200

    lvl 3 condition et rajout de condition dans NomMode

--- src/LightBot/NomMode.java
+++ src/LightBot/NomMode.java
@@ -4,6 +4,7 @@ public enum NomMode {
 		Basic(NomMode.class.getResource("levels/basic/").getPath()),
 		Procedure(NomMode.class.getResource("levels/procedure/").getPath()),
 		Loop(NomMode.class.getResource("levels/loop/").getPath()),
+		Condition(NomMode.class.getResource("levels/condition/").getPath()),
 		Memory(NomMode.class.getResource("levels/memory/").getPath()),
 		Fork(NomMode.class.getResource("levels/fork/").getPath()),
 		Versus_IA(NomMode.class.getResource("levels/ia/").getPath());
