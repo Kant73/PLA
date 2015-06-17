15e96f96f536ecff8d8fbc5f59b288f6512e19f1
P0ppoff <jules.hablot@gmail.com>
2015-06-17 11:15:25 +0200

    Modification d'avancer

--- src/LightBot/actions/Avancer.java
+++ src/LightBot/actions/Avancer.java
@@ -1,7 +1,9 @@
 package LightBot.actions;
 
 import LightBot.Terrain;
+import LightBot.cases.Case;
 import LightBot.cases.Couleur;
+import LightBot.cases.Transparente;
 import LightBot.personnage.Pcardinaux;
 import LightBot.personnage.Personnage;
 
@@ -23,35 +25,62 @@ public class Avancer extends Actions {
 	@Override
 	public void agir() {
 		if(super.matchColor()){
-			Pcardinaux o = this.perso.getOrientation();
 			Terrain T = this.perso.getTerrain();
 			int x = this.perso.getPositionX();
 			int y = this.perso.getPositionY();
 			int h = T.getEnsembleDeCase()[x][y].getHauteur();
 			
-			switch(o){
-				case EAST :
-					if ((x+1)<T.getLargeur()){
-						if(h == T.getEnsembleDeCase()[x+1][y].getHauteur())this.perso.setPositionX(x+1);
-					}else {this.perso.setMort(true);}
-					break;
-				case SOUTH :
-					if ((y+1)<T.getLongueur()){
-						if(h == T.getEnsembleDeCase()[x][y+1].getHauteur())this.perso.setPositionY(y+1);
-					}else{this.perso.setMort(true);}
-					break;
-				case WEST : 
-					if ((x-1)>=0){
-						if(h == T.getEnsembleDeCase()[x-1][y].getHauteur())this.perso.setPositionX(x-1);
-					}else{this.perso.setMort(true);}
-					break;
-				case NORTH :
-					if ((y-1)>=0 ){
-						if(h == T.getEnsembleDeCase()[x][y-1].getHauteur())this.perso.setPositionY(y-1);
-					}else{System.out.println(x+"  "+y);
-						this.perso.setMort(true);}
-					break;
-				default:break;
+			Case devant;
+			switch(this.perso.getOrientation()){
+			case EAST:
+				if((x+1) < T.getLargeur()){
+					devant = T.getEnsembleDeCase()[x+1][y];
+					if(devant instanceof Transparente || (h == devant.getHauteur())){
+						this.perso.setPositionX(x+1);
+					}else if(devant.getHauteur() == 0){
+						this.perso.setMort(true);
+					}
+				}else{
+					this.perso.setMort(true);
+				}
+				break;
+			case SOUTH:
+				if((y+1) < T.getLongueur()){
+					devant = T.getEnsembleDeCase()[x][y+1];
+					if(devant instanceof Transparente || (h == devant.getHauteur())){
+						this.perso.setPositionY(y+1);
+					}else if(devant.getHauteur() == 0){
+						this.perso.setMort(true);
+					}
+				}else{
+					this.perso.setMort(true);
+				}
+				break;
+			case WEST:
+				if ((x-1) >= 0){
+					devant = T.getEnsembleDeCase()[x-1][y];
+					if(devant instanceof Transparente || (h == devant.getHauteur())){
+						this.perso.setPositionX(x-1);
+					}else if(devant.getHauteur() == 0){
+						this.perso.setMort(true);
+					}
+				}else{
+					this.perso.setMort(true);
+				}
+				break;
+			case NORTH:
+				if ((y-1) >= 0){
+					devant = T.getEnsembleDeCase()[x][y-1];
+					if(devant instanceof Transparente || (h == devant.getHauteur())){
+						this.perso.setPositionY(y-1);
+					}else if(devant.getHauteur() == 0){
+						this.perso.setMort(true);
+					}
+				}else{
+					this.perso.setMort(true);
+				}
+				break;	
+			default:break;
 			}
 		}
 	}
