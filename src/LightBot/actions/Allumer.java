2a4dc89728ccbeaeb7719878e18afecc55227e04
Mugiwara No Luffy <Mugiwara No Luffy@MugiwaraNoLuffy>
2015-06-18 09:37:50 +0200

    update Allumer et Creation d'allumerbattle

--- src/LightBot/actions/Allumer.java
+++ src/LightBot/actions/Allumer.java
@@ -5,8 +5,6 @@ import LightBot.cases.Condition;
 import LightBot.cases.Couleur;
 import LightBot.cases.Lampe;
 import LightBot.cases.Pointeur;
-import LightBot.cases.PointeurTri;
-import LightBot.cases.Transparente;
 import LightBot.personnage.Personnage;
 
 
@@ -29,26 +27,6 @@ public class Allumer extends Actions {
 			return "Allumer";
 		}
 	}
-	
-	public void agirBattle(){
-		Case C = this.perso.getTerrain().getEnsembleDeCase()[this.perso.getPositionX()][this.perso.getPositionY()];
-		Couleur color = this.perso.getCouleur();
-		int nbLampeAllumee=this.perso.getTerrain().getNbLampeAllumee();
-		if (C.getColor() == color){
-			C.setColor(Couleur.Bleu);
-			this.perso.getTerrain().setNbLampeAllumee(nbLampeAllumee-1);
-		}
-		else {
-			C.setColor(color);
-			this.perso.getTerrain().setNbLampeAllumee(nbLampeAllumee+1);
-		}
-	}
-	
-	@Override
-	public Allumer clone() throws CloneNotSupportedException{
-		Allumer copie=(Allumer)super.clone();
-		return copie;
-	}
 
 	@Override
 	public void agir() {
@@ -67,52 +45,17 @@ public class Allumer extends Actions {
 					break;
 				default:break;
 				}
-			}else if (C instanceof PointeurTri){
-				if(((PointeurTri)C).estDepart()){
-					if(tableauTrie(C)){
-						Lampe arrivee = ((PointeurTri) C).getArrivee();
-						this.perso.setPosition(this.perso.getTerrain().getPosCaseX(arrivee), this.perso.getTerrain().getPosCaseY(arrivee));
-						victoire();
-					}else{
-						Case suivante = ((Pointeur) C).getSuivante();
-						if(suivante != null){
-							this.perso.setPosition(this.perso.getTerrain().getPosCaseX(suivante), this.perso.getTerrain().getPosCaseY(suivante));
-						}
-					}
-				}
-			}else if (C instanceof Pointeur){
-				Case temp = ((Pointeur) C).getSuivante();
-				if (temp != null){
+			}
+			else if (C instanceof Pointeur){
+				Case temp;
+				temp = ((Pointeur) C).getSuivante();
+				if (temp != null)
 					this.perso.setPosition(this.perso.getTerrain().getPosCaseX(temp), this.perso.getTerrain().getPosCaseY(temp));
-				}
-			}else if(C instanceof Condition){
+			}
+			else if(C instanceof Condition){
 				if(this.perso.getCouleur()!=Couleur.Blanc)this.perso.setCouleur(Couleur.Blanc);
 				else this.perso.setCouleur(C.getColor());
 			}
 		}
-	}
-	
-	/* Convention : le terrain d'un tri doit avoir le tableau à trier en y = 2 et la lampe en (0,0) */
-	private boolean tableauTrie(Case pointeur){
-		boolean estTrie = true;
-		int xInit = 1;
-		int xFin = this.perso.getTerrain().getPosCaseX(pointeur);
-		while(estTrie && (xInit < xFin-1)){
-			Case courante = this.perso.getTerrain().getEnsembleDeCase()[xInit][2];
-			Case prochaine = this.perso.getTerrain().getEnsembleDeCase()[xInit+1][2];
-			estTrie &= courante.getHauteur()+1 == prochaine.getHauteur();
-			xInit++;
-		}
-		return estTrie;
-	}
-	
-	private void victoire(){
-		for(int x = 0; x<this.perso.getTerrain().getLargeur(); x++){
-			Case courante = this.perso.getTerrain().getEnsembleDeCase()[x][2];
-			if(courante instanceof Transparente){
-				this.perso.getTerrain().getEnsembleDeCase()[x][2] = ((Transparente)courante).toNormal();
-			}
-		}
-		new Allumer(this.perso).agir();
 	}
 }
---
+++ src/LightBot/actions/AllumerBattle.java
@@ -0,0 +1,29 @@
+package LightBot.actions;
+
+import LightBot.cases.Case;
+import LightBot.cases.Couleur;
+import LightBot.personnage.Personnage;
+
+public class AllumerBattle extends Actions {
+
+	public AllumerBattle(Personnage p) {
+		super(p);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	@Override
+	public void agir(){
+		Case C = this.perso.getTerrain().getEnsembleDeCase()[this.perso.getPositionX()][this.perso.getPositionY()];
+		Couleur color = this.perso.getCouleur();
+		int nbLampeAllumee=this.perso.getTerrain().getNbLampeAllumee();
+		if (C.getColor() == color){
+			C.setColor(Couleur.Bleu);
+			this.perso.getTerrain().setNbLampeAllumee(nbLampeAllumee-1);
+		}
+		else {
+			C.setColor(color);
+			this.perso.getTerrain().setNbLampeAllumee(nbLampeAllumee+1);
+		}
+	}
+}
